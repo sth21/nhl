9cbdc008b9730078370799c35182f89a8bb974ec
Sam <bills2021e@gmail.com>
2023-02-21 10:38:56 -0500

    Initialize React and Firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClgxt7RGfjNp8jo5XaJU_CzNNdqwJGe7Q",
+  authDomain: "nhl-website.firebaseapp.com",
+  projectId: "nhl-website",
+  storageBucket: "nhl-website.appspot.com",
+  messagingSenderId: "180172327358",
+  appId: "1:180172327358:web:415e84328d59dcb2add1ef"
+};
+
+const APP = initializeApp(firebaseConfig);
