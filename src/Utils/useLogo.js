b002330f8e71f652f96365e8c230dc5a68e110d8
Sam <bills2021e@gmail.com>
2023-02-23 21:13:37 -0500

    Move logic from Scoreboard to ScoreboardGame in efforts to simplify code

---
+++ src/Utils/useLogo.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+import { DB } from '../firebase';
+import { doc, getDoc } from "firebase/firestore";
+
+export default function useLogo(id) {
+    const [ logo, setLogo ] = useState(null)
+
+    // Fetch url from id
+    useEffect(() => {
+        if ( !id ) return;
+
+        const fetchLogo = async () => {
+            const logoDoc = await getDoc(doc(DB, "team-logos", id));
+            const logo = logoDoc.data().logo;
+            setLogo(logo);
+        };
+        fetchLogo();
+    }, [ id ]);
+
+    return logo;
+}
