71a93311138ec6b175ccbbc8e51a0ee3bc5f7827
Sam <bills2021e@gmail.com>
2023-03-07 09:47:39 -0500

    Add table to Draft widget

--- src/Components/Home/Widgets/DraftWidget.js
+++ src/Components/Home/Widgets/DraftWidget.js
@@ -1,23 +1,39 @@
+import { Table, TableBody, TableHead, TableRow } from '@mui/material';
+import { StyledHeader, StyledTableCell, StyledTableHeader, StyledTableContainer } from '../../../StyledComponents/Home/WidgetComponents';
+
 import uniqid from 'uniqid';
 
 export default function DraftWidget(props) {
     
     const firstPickOdds = [ "25.5%", "13.5%", "11.5%", "9.5%", "8.5%", "7.5%", "6.5%", "6.0%", "5.0%", "3.5%", "3.0%"]
     
-    const teams = [...props.teams.slice(20)].reverse();
+    const teams = [...props.teams.slice(21)].reverse();
 
     return (
         <div>
-            <p>Draft Widget</p>
-            {
-                teams.map((team, index) => (
-                    <div key = { uniqid() }>
-                        <p>{ index + 1 }</p>
-                        <img src={ (props.logos) ? props.logos[parseInt(team.team.id, 10)] : "#" } alt="Team"></img>
-                        <p>{ firstPickOdds[index] }</p>
-                    </div>
-                ))
-            }
+            <StyledHeader>Lottery Odds</StyledHeader>
+            <StyledTableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableHeader>Rank</StyledTableHeader>
+                            <StyledTableHeader>Team</StyledTableHeader>
+                            <StyledTableHeader>Odds</StyledTableHeader>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            teams.map((team, index) => (
+                                <TableRow key = { uniqid() }>
+                                    <StyledTableCell>{ index + 1 }</StyledTableCell>
+                                    <StyledTableCell><img src={ (props.logos) ? props.logos[parseInt(team.team.id, 10)] : "#" } alt="Team"></img></StyledTableCell>
+                                    <StyledTableCell>{ firstPickOdds[index] }</StyledTableCell>
+                                </TableRow>
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+            </StyledTableContainer>
         </div>
     );
     
