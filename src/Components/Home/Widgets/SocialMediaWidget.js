0dcea2d561c91192a433bc44519649610eae7222
Sam <bills2021e@gmail.com>
2023-03-11 21:23:05 -0500

    Finalize styling and add link targets for SM widget

--- src/Components/Home/Widgets/SocialMediaWidget.js
+++ src/Components/Home/Widgets/SocialMediaWidget.js
@@ -1,62 +1,72 @@
-import Twitter from './../../../Media/twitter.png';
-import Facebook from './../../../Media/facebook.png';
-import Instagram from './../../../Media/instagram.png';
-import Giphy from './../../../Media/giphy.png';
-import Newsletter from './../../../Media/newsletter.png';
-import Podcast from './../../../Media/podcast.png';
-import Twitch from './../../../Media/twitch.png';
-import Youtube from './../../../Media/youtube.png';
-import Tiktok from './../../../Media/tiktok.png';
-import Snapchat from './../../../Media/snapchat.png';
+import Twitter from "./../../../Media/twitter.png";
+import Facebook from "./../../../Media/facebook.png";
+import Instagram from "./../../../Media/instagram.png";
+import Giphy from "./../../../Media/giphy.png";
+import Newsletter from "./../../../Media/newsletter.png";
+import Podcast from "./../../../Media/podcast.png";
+import Twitch from "./../../../Media/twitch.png";
+import Youtube from "./../../../Media/youtube.png";
+import Tiktok from "./../../../Media/tiktok.png";
+import Snapchat from "./../../../Media/snapchat.png";
 
+import {
+  StyledHeader,
+  StyledSMLink,
+  StyledSMWrapper,
+} from "../../../StyledComponents/Home/WidgetComponents";
 
 export default function SocialMediaWidget(props) {
-    
-    return (
-        <div>
-            <p>Follow us</p>
-            <div>
-                <div>
-                    <img src={Twitter} alt="logo"></img>
-                    <p>Twitter</p>
-                </div>
-                <div>
-                    <img src={Facebook} alt="logo"></img>
-                    <p>Facebook</p>
-                </div>
-                <div>
-                    <img src={Instagram} alt="logo"></img>
-                    <p>Instagram</p>
-                </div>
-                <div>
-                    <img src={Tiktok} alt="logo"></img>
-                    <p>Tiktok</p>
-                </div>
-                <div>
-                    <img src={Youtube} alt="logo"></img>
-                    <p>Youtube</p>
-                </div>
-                <div>
-                    <img src={Twitch} alt="logo"></img>
-                    <p>Twitch</p>
-                </div>
-                <div>
-                    <img src={Snapchat} alt="logo"></img>
-                    <p>Snapchat</p>
-                </div>
-                <div>
-                    <img src={Giphy} alt="logo"></img>
-                    <p>Giphy</p>
-                </div>
-                <div>
-                    <img src={Podcast} alt="logo"></img>
-                    <p>Podcast</p>
-                </div>
-                <div>
-                    <img src={Newsletter} alt="logo"></img>
-                    <p>Newsletter</p>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <StyledHeader>Follow NHL</StyledHeader>
+      <StyledSMWrapper>
+        <StyledSMLink to="https://www.twitter.com/nhl" target="_blank">
+          <img src={Twitter} alt="logo"></img>
+          <p>Twitter</p>
+        </StyledSMLink>
+        <StyledSMLink to="https://www.facebook.com/NHL/" target="_blank">
+          <img src={Facebook} alt="logo"></img>
+          <p>Facebook</p>
+        </StyledSMLink>
+        <StyledSMLink to="https://www.instagram.com/nhl/" target="_blank">
+          <img src={Instagram} alt="logo"></img>
+          <p>Instagram</p>
+        </StyledSMLink>
+        <StyledSMLink to="https://www.tiktok.com/@nhl?lang=en" target="_blank">
+          <img src={Tiktok} alt="logo"></img>
+          <p>Tiktok</p>
+        </StyledSMLink>
+        <StyledSMLink to="https://www.youtube.com/nhl" target="_blank">
+          <img src={Youtube} alt="logo"></img>
+          <p>Youtube</p>
+        </StyledSMLink>
+        <StyledSMLink to="https://www.twitch.tv/nhl" target="_blank">
+          <img src={Twitch} alt="logo"></img>
+          <p>Twitch</p>
+        </StyledSMLink>
+        <StyledSMLink to="https://www.snapchat.com/add/nhl" target="_blank">
+          <img src={Snapchat} alt="logo"></img>
+          <p>Snapchat</p>
+        </StyledSMLink>
+        <StyledSMLink to="https://www.giphy.com/nhl" target="_blank">
+          <img src={Giphy} alt="logo"></img>
+          <p>Giphy</p>
+        </StyledSMLink>
+        <StyledSMLink
+          to="https://www.nhl.com/multimedia/podcasts"
+          target="_blank"
+        >
+          <img src={Podcast} alt="logo"></img>
+          <p>Podcasts</p>
+        </StyledSMLink>
+        <StyledSMLink
+          to="https://jebbit.nhl.com/k38ek6ta?L=Owned+Web&JC=leftrail"
+          target="_blank"
+        >
+          <img src={Newsletter} alt="logo"></img>
+          <p>Newsletter</p>
+        </StyledSMLink>
+      </StyledSMWrapper>
+    </div>
+  );
 }
--- src/StyledComponents/Home/WidgetComponents.js
+++ src/StyledComponents/Home/WidgetComponents.js
@@ -3,76 +3,94 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledWidgetContainer = styled.div`
-    display: grid;
-    padding: 3em;
-    grid-template: auto / repeat(auto-fit, minmax(400px, 1fr));
-    grid-auto-flow: row;
-    gap: 3em;
+  display: grid;
+  padding: 3em;
+  grid-template: auto / repeat(auto-fit, minmax(400px, 1fr));
+  grid-auto-flow: row;
+  gap: 3em;
 
-    & > div {
-        background: var(--white);
-        padding: 2em;
-        box-shadow: 0 2px 3px rgb(0 0 0 / 10%);
-    }
+  & > div {
+    background: var(--white);
+    padding: 2em;
+    box-shadow: 0 2px 3px rgb(0 0 0 / 10%);
+  }
 
-    & img {
-        height: 2.5em;
-        width: 2.5em;
-    }
+  & img {
+    height: 2.5em;
+    width: 2.5em;
+  }
 `;
 
 const StyledHeadlineWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 3em;
-    padding: 1em 0em;
-    vertical-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 3em;
+  padding: 1em 0em;
+  vertical-align: center;
 `;
 
 const StyledHeader = styled.h4`
-    font-weight: bold;
-    font-size: 1.1rem;
-    width: 100%;
-    padding-bottom: 1em;
-    border-bottom: 1px solid var(--grey);
+  font-weight: bold;
+  font-size: 1.1rem;
+  width: 100%;
+  padding-bottom: 1em;
+  border-bottom: 1px solid var(--grey);
 `;
 
 const StyledLink = styled(Link)`
-    color: inherit;
-    font-weight: inherit;
-    text-decoration: none;
+  color: inherit;
+  font-weight: inherit;
+  text-decoration: none;
 
-    &:hover {
+  &:hover {
     text-decoration: none;
-    }
+  }
 
-    &:visited {
+  &:visited {
     text-decoration: none;
-    }
+  }
 
-    &:active {
+  &:active {
     text-decoration: none;
-    }
+  }
 `;
 
 const StyledTableCell = styled(TableCell)`
-    text-align: center !important;
-    padding: .625em !important;
-    font: inherit !important;
-    font-size: .9rem !important;
-    border-bottom: 1px solid var(--grey) !important;
+  text-align: center !important;
+  padding: 0.625em !important;
+  font: inherit !important;
+  font-size: 0.9rem !important;
+  border-bottom: 1px solid var(--grey) !important;
 `;
 
 const StyledTableHeader = styled(StyledTableCell)`
-    font-weight: bold !important;
-    font-size: inherit;
+  font-weight: bold !important;
+  font-size: inherit;
 `;
 
 const StyledTableContainer = styled(TableContainer)`
-    padding: 1em 0em;
-`; 
+  padding: 1em 0em;
+`;
 
-export { 
-    StyledWidgetContainer, StyledHeadlineWrapper, StyledHeader, StyledLink, StyledTableCell, 
-    StyledTableHeader, StyledTableContainer
+const StyledSMWrapper = styled(StyledHeadlineWrapper)`
+  gap: 1.8em;
+`;
+
+const StyledSMLink = styled(StyledLink)`
+  display: flex;
+  gap: 1em;
+  font-weight: bold;
+  align-items: center;
+`;
+
+export {
+  StyledWidgetContainer,
+  StyledHeadlineWrapper,
+  StyledHeader,
+  StyledLink,
+  StyledTableCell,
+  StyledTableHeader,
+  StyledTableContainer,
+  StyledSMWrapper,
+  StyledSMLink,
 };
