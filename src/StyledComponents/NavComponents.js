9fe783708f75e807f0ec8765da1c14ecbac7cc8f
Sam <bills2021e@gmail.com>
2023-03-12 15:54:37 -0400

    Make nav bar sticky

--- src/StyledComponents/NavComponents.js
+++ src/StyledComponents/NavComponents.js
@@ -2,55 +2,65 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const StyledNav = styled.nav`
-    display: flex;
-    color: var(--white);
-    font-weight: bold;
-    padding: 1em 3em;
-    background: var(--black);
-    gap: 3em;
-    min-height: 60px;
-    align-items: center;
-    max-width: 100%;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  display: flex;
+  color: var(--white);
+  font-weight: bold;
+  padding: 1em 3em;
+  background: var(--black);
+  gap: 3em;
+  min-height: 60px;
+  align-items: center;
+  max-width: 100%;
 `;
 
 const StyledLogo = styled.img`
-    height: 4.5em;
-    width: 4.5em;
+  height: 4.5em;
+  width: 4.5em;
 `;
 
 const StyledIcon = styled(StyledLogo)`
-    height: 1.5em;
-    width: 1.5em;
+  height: 1.5em;
+  width: 1.5em;
 `;
 
 const StyledNavWrapper = styled.div`
-    display: flex;
-    gap: 3em;
-    align-items: center;
-    margin-right: ${ props => props.pushRight ? "auto" : 0 };
+  display: flex;
+  gap: 3em;
+  align-items: center;
+  margin-right: ${(props) => (props.pushRight ? "auto" : 0)};
 `;
 
 const StyledLink = styled(Link)`
-    color: inherit;
-    font-weight: inherit;
+  color: inherit;
+  font-weight: inherit;
+  text-decoration: none;
+
+  &:hover {
     text-decoration: none;
-  
-    &:hover {
-      text-decoration: none;
-    }
-  
-    &:visited {
-      text-decoration: none;
-    }
-  
-    &:active {
-      text-decoration: none;
-    }
+  }
+
+  &:visited {
+    text-decoration: none;
+  }
+
+  &:active {
+    text-decoration: none;
+  }
 `;
 
 const StyledLoginContainer = styled(StyledNavWrapper)`
-    gap: 1em;
-    margin-right: 0;
+  gap: 1em;
+  margin-right: 0;
 `;
 
-export { StyledNav, StyledLogo, StyledIcon, StyledNavWrapper, StyledLink, StyledLoginContainer };
+export {
+  StyledNav,
+  StyledLogo,
+  StyledIcon,
+  StyledNavWrapper,
+  StyledLink,
+  StyledLoginContainer,
+};
